c4525c2d1a16371bfa33c9296130748bbea81fc0
Aniket <aninamdeo@gmail.com>
2023-05-04 23:43:42 +0530

    hi

---
+++ storage/framework/views/5d5d7e48f5808120acb76aa22aae0c98.php
@@ -0,0 +1,13 @@
+<?PHP
+$world = 20;
+?>
+
+
+
+
+
+<?php if($world>20): ?>
+    <?php echo e('hi'); ?>
+
+<?php endif; ?>
+<?php echo e('Not'); ?><?php /**PATH C:\xampp\htdocs\laravel_project\resources\views/test.blade.php ENDPATH**/ ?>
